9e553d9de2f641b5eb6135e9e39fb70743d9ac7c
jeffAntonis <antunis356@gmail.com>
2019-03-23 20:24:24 -0300

    Implementado navegação tab_bottom

--- app/src/main/java/cbridge/android/adouble/com/cbridge/MainActivity.java
+++ app/src/main/java/cbridge/android/adouble/com/cbridge/MainActivity.java
@@ -1,53 +1,76 @@
 package cbridge.android.adouble.com.cbridge;
 
-import android.app.Activity;
-import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView nav_bottom;
+    private FrameLayout container;
+
+    private ChatFragment chatFragment;
+    private ProjectsFragment projectsFragment;
+    private PublishFragment publishFragment;
+    private SearchFragment searchFragment;
+    private ProfileFragment profileFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final EditText txtEmail = findViewById(R.id.txtEmail);
-        final EditText txtPassword = findViewById(R.id.txtPassword);
+        container = (FrameLayout) findViewById(R.id.container);
+        nav_bottom = (BottomNavigationView) findViewById(R.id.bottom_navgation);
 
-        Button btnLogin = findViewById(R.id.btn_login);
-        btnLogin.setOnClickListener(new View.OnClickListener() {
+        chatFragment = new ChatFragment();
+        projectsFragment = new ProjectsFragment();
+        publishFragment = new PublishFragment();
+        searchFragment = new SearchFragment();
+        profileFragment = new ProfileFragment();
+
+        nav_bottom.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View v) {
-                login(txtEmail.getText().toString(), txtPassword.getText().toString(), MainActivity.this);
-            }
-        });
-    }
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
-    private void login(String email, String password, final Activity activity) {
-        final FirebaseAuth auth = FirebaseAuth.getInstance();
-        Task<AuthResult> task = auth.signInWithEmailAndPassword(email, password);
-        task.addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(Task<AuthResult> task) {
-                if (task.isSuccessful()) {
-                    FirebaseUser user = auth.getCurrentUser();
-                    Toast.makeText(activity, user.getUid() + " / " + user.getEmail(), Toast.LENGTH_LONG).show();
+                switch (item.getItemId()){
 
-                } else {
-                    Toast.makeText(activity, "Falha na autenticação", Toast.LENGTH_LONG).show();
+                    case R.id.nav_account :
+                        setFragment(profileFragment);
+                        return true;
+
+                    case R.id.nav_project :
+                        setFragment(projectsFragment);
+                        return true;
+
+                    case R.id.nav_add :
+                        setFragment(publishFragment);
+                        return true;
+
+                    case R.id.nav_search :
+                        setFragment(searchFragment);
+                        return true;
+
+                    case R.id.nav_message :
+                        setFragment(chatFragment);
+                        return true;
+
+                    default:
+                        return false;
                 }
             }
         });
+
+        nav_bottom.setSelectedItemId(R.id.nav_message);
+    }
+
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.container, fragment);
+        fragmentTransaction.commit();
     }
 }
